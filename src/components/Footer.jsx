7e2bf06905ea0d092b2666a1dfe819f7d98cf897
TomD4vs <elyton.moreira163@gmail.com>
2026-03-11 20:57:54 -0400

    add date footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer>
-      <small>&copy; 2026 - Módulo 04 - Desenvolvimento Front-end com React - Iteam</small>
+      <small>&copy; {new Date().getFullYear()} - Módulo 04 - Desenvolvimento Front-end com React - Iteam</small>
     </footer>
   );
 }
