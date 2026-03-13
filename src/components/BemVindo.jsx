2b2847a7c489f381cd037721b18c19c0a2f60425
TomD4vs <elyton.moreira163@gmail.com>
2026-03-13 14:25:50 -0400

    feat: removendo contador

--- src/components/BemVindo.jsx
+++ src/components/BemVindo.jsx
@@ -1,15 +1,9 @@
-const BemVindo = ({ nomeUsuario, totalHabitos }) => {
+const BemVindo = ({ nomeUsuario }) => {
   const nomeFormatado = nomeUsuario.toUpperCase();
-
-  const mensagem =
-    totalHabitos > 0
-      ? `Você tem ${totalHabitos} hábitos cadastrados!`
-      : `Nenhum hábito cadastrado ainda. Vamos criar o primeiro?`;
 
   return (
     <div>
       <h2>Olá, {nomeFormatado}!</h2>
-      <p>{mensagem}</p>
     </div>
   );
 }
