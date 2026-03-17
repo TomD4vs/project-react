178c6072e8f914a7eb26872d44518b7c7a4497d4
TomD4vs <elyton.moreira163@gmail.com>
2026-03-17 14:50:37 -0400

    feat: aplicando conceitos de useEffect

--- src/components/HabitList.jsx
+++ src/components/HabitList.jsx
@@ -1,19 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import HabitCard from "./HabitCard";
 
 function HabitList({ }) {
 
-    const [habits, setHabits] = useState([
-        { id: 1, nome: "Exercício", descricao: 'Treino de Força', meta: 5, ativo: true, diasFeitos: 5 },
-        { id: 2, nome: "Leitura", descricao: 'Livro ou artigo', meta: 1, ativo: false, diasFeitos: 3 },
-        { id: 3, nome: "Meditação", descricao: 'Respiração e foco', meta: 3, ativo: true, diasFeitos: 2 },
-        { id: 4, nome: "Hidratação", descricao: 'Beber 2L de água', meta: 4, ativo: false, diasFeitos: 6 },
-    ])
+    const [habits, setHabits] = useState(() => {
+        const stored = localStorage.getItem('my-daily-habits')
+        
+        if(!stored) return [
+            { id: 1, nome: "Exercício", descricao: 'Treino de Força', meta: 5, ativo: true, diasFeitos: 5 },
+            { id: 2, nome: "Leitura", descricao: 'Livro ou artigo', meta: 1, ativo: false, diasFeitos: 3 },
+            { id: 3, nome: "Meditação", descricao: 'Respiração e foco', meta: 3, ativo: true, diasFeitos: 2 },
+            { id: 4, nome: "Hidratação", descricao: 'Beber 2L de água', meta: 4, ativo: false, diasFeitos: 6 },
+        ]
+
+        //se há dados salvos - tenta fazer o parse
+        try{
+            return JSON.parse(stored)
+        } catch {
+            // se o json estiver corrompido volta para o array inicial
+            return []
+        }
+    })
 
     const [novoNome, setNovoNome] = useState('')
     const [novaDescricao, setnovaDescricao] = useState('')
     const [novaMeta, setnovaMeta] = useState('')
     const [novaCategoria, setnovaCategoria] = useState('')
+
+    //conceitos de useEffect
+    useEffect(() => {
+        localStorage.setItem('my-daily-habits', JSON.stringify(habits))
+    }, [habits])
+
 
     const adicionarHabit = (event) => {
         event.preventDefault()
@@ -47,7 +65,16 @@ function HabitList({ }) {
 
     }
 
+    const limparHistorico = () =>{
+        localStorage.removeItem('my-daily-habits')
+        setHabits([
+            { id: 1, nome: "Exercício", descricao: 'Treino de Força', meta: 5, ativo: true, diasFeitos: 5 },
+            { id: 2, nome: "Leitura", descricao: 'Livro ou artigo', meta: 1, ativo: false, diasFeitos: 3 },
+        ])
+    }
+
     return (
+        
         <section>
             <form onSubmit={adicionarHabit} className="habit-form">
                 <div>
@@ -96,8 +123,8 @@ function HabitList({ }) {
                 <button type="submit">Adicionar hábito</button>
             </form>
 
-
             <ul>
+                <button onClick={limparHistorico}>Limpar Historico</button>
                 {habits.length === 0
                     ? <p>Nenhum hábito cadastrado ainda. Que tal começar?</p>
                     : <p>Você tem {habits.length} hábito(s) cadastrado(s).</p>
