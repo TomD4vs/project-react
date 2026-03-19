2e4cafc7d4e3a34ca75c0edffb52f2430de9ee72
TomD4vs <elyton.moreira163@gmail.com>
2026-03-19 15:44:01 -0400

    feat: update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,15 @@
 function Footer() {
   return (
     <footer>
-      <small>&copy; {new Date().getFullYear()} - Módulo 04 - Desenvolvimento Front-end com React - Iteam</small>
+      <small>
+        © {new Date().getFullYear()} - Desenvolvimento Front-end com React
+        {' · '}
+        <a href="https://github.com/TomD4vs" target="_blank" rel="noreferrer">
+          TomD4vs
+        </a>
+      </small>
     </footer>
-  );
+  )
 }
 
-export default Footer;
+export default Footer
